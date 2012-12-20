954e7fe58cc53377d1a7792f8d28c2d1826a9f2b
Mahi Fernando <mahifernando@gmail.com>
2012-12-20 18:52:20 +0000

    Add Clients to Entity.

---
+++ GaugesNet/Entity/ApiClients.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GaugesNet.Entity
+{
+    public class ApiClients
+    {
+        [JsonProperty("client")]
+        public Client Client { get; set; }
+
+        [JsonProperty("clients")]
+        public Clients Clients { get; set; }
+
+    }
+
+    public class Client
+    {
+        [JsonProperty("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonProperty("urls")]
+        public Urls Urls { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+    }
+
+    public class Clients : List<Client>
+    {
+
+    }
+}
